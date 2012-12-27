c83bf45422594cdcd8f99566d33512e8d0224b30
mz121star <mz121star@gmail.com>
2012-12-27 16:29:23 +0800

    added jasmine

--- test/route.test.js
+++ test/route.test.js
@@ -1,5 +1,31 @@
-describe("A suite", function() {
-    it("contains spec with an expectation", function() {
-        expect(true).toBe(true);
+describe("JMvc route", function() {
+
+    beforeEach(function() {
+
+        var route=JMvc.Route.Create({
+            Paths:{
+                "!/index":"index",
+                "!/about":"about",
+                "!/product":"product"
+            },
+            index:function(){
+              document.body.innerHTML="<h1>首页显示</h1>";
+            },
+            about:function(){
+                document.body.innerHTML="<h1>dalian</h1>";
+            },
+            product:function(){
+               document.body.innerHTML="<ul><li>iphone</li></ul>";
+
+
+            }
+        }).Start();
+    });
+
+    it("hash changed", function() {
+        window.location.hash="!/about";
+        var html=document.body.innerHTML;
+        console.log("neirong:"+html);
+        expect(html).toEqual("<h1>dalian</h1>");
     });
 });
