2e074a6d84e899cbc68257e837bb8ae380cdd457
mz121star <mz121star@gmail.com>
2013-01-07 18:01:21 +0800

    node http module

---
+++ build/Mhttp.js
@@ -0,0 +1,34 @@
+var http = require("http");
+(function () {
+    var mhttp;
+    mhttp = {
+        get:function (url) {
+            var scuessCallbacks=[],
+                errorCallbacks = [];
+            var deferred = {
+                done:function (callback) {
+                   if( typeof callback==='function') scuessCallbacks.push(callback);
+                    return this;
+                },
+                fail:function (callback) {
+                    if( typeof callback==='function') errorCallbacks.push(callback);
+                    return this;
+                }
+            }
+            http.get(url, function (res) {
+                res.on('data',function (d) {
+                    for (i = 0, n = scuessCallbacks.length; i < n; i++) {
+                        scuessCallbacks[i](d);
+                        console.log("scuess lenght:"+scuessCallbacks.length);
+                    }
+                }).on('error', function (e) {
+                        for (i = 0, n = errorCallbacks.length; i < n; i++) {
+                            errorCallbacks[i](e);
+                        }
+                 });
+            });
+            return deferred;
+        }
+    }
+    module.exports = mhttp;
+})(module);
